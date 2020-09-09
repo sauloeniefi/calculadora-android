d6f59cc0a4c026e8a1a1cca970e1048f3c5867be
sauloeniefi <sauloeniefi@gmail.com>
2020-09-09 14:49:35 -0300

    Ajuste validações

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -16,43 +16,49 @@ class MainActivity : AppCompatActivity() {
 
     fun calcular(componente: View) {
 
-        val nome = etNome.text.toString()
-        val nota1 = etNota1.text.toString().toInt()
-        val nota2 = etNota2.text.toString().toInt()
-        val mediaNecessaria = etMedia.text.toString().toInt()
-        val mediaFinal = (nota1 + nota2) / 2
-
-        val corReprovado = "#933d41"
-        val corNaMedia = "#daa520"
-        val corAprovado = "#2e8b57"
-
-        val fraseReprovado = "Olá $nome, sua média foi $mediaFinal. Você está reprovado"
-        val fraseNaMedia = "Olá $nome, sua média foi $mediaFinal. Você está aprovado, mas passou raspando!"
-        val fraseAprovado = "Olá $nome, sua média foi $mediaFinal. Você está aprovado. Parabéns!"
-
-        if (mediaNecessaria > 10) {
-            Toast.makeText(this, "Apenas valores entre 0 e 10", Toast.LENGTH_SHORT).show()
-            tvResultado.visibility = View.GONE
+        if (etNome.text.isEmpty() || etNota1.text.isEmpty() || etNota2.text.isEmpty() || etMedia.text.isEmpty()) {
+            Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
         } else {
-            if (mediaFinal < mediaNecessaria) {
-                tvResultado.text = fraseReprovado
-                tvResultado.setTextColor(Color.parseColor(corReprovado))
-                tvResultado.visibility = View.VISIBLE
-            } else if (mediaFinal == mediaNecessaria) {
-                if (mediaNecessaria == 10) {
+            val nome = etNome.text.toString()
+            val nota1 = etNota1.text.toString().toInt()
+            val nota2 = etNota2.text.toString().toInt()
+            val mediaNecessaria = etMedia.text.toString().toInt()
+            val mediaFinal = (nota1 + nota2) / 2
+
+            val corReprovado = "#933d41"
+            val corNaMedia = "#daa520"
+            val corAprovado = "#2e8b57"
+
+            val fraseReprovado = "Olá $nome, sua média foi $mediaFinal. Você está reprovado"
+            val fraseNaMedia =
+                "Olá $nome, sua média foi $mediaFinal. Você está aprovado, mas passou raspando!"
+            val fraseAprovado =
+                "Olá $nome, sua média foi $mediaFinal. Você está aprovado. Parabéns!"
+
+            if (mediaNecessaria > 10) {
+                Toast.makeText(this, "Apenas valores entre 0 e 10", Toast.LENGTH_SHORT).show()
+                tvResultado.visibility = View.GONE
+            } else {
+                if (mediaFinal < mediaNecessaria) {
+                    tvResultado.text = fraseReprovado
+                    tvResultado.setTextColor(Color.parseColor(corReprovado))
+                    tvResultado.visibility = View.VISIBLE
+                } else if (mediaFinal == mediaNecessaria) {
+                    if (mediaNecessaria == 10) {
+                        tvResultado.text = fraseAprovado
+                        tvResultado.setTextColor(Color.parseColor(corAprovado))
+                        tvResultado.visibility = View.VISIBLE
+                    } else {
+                        tvResultado.text = fraseNaMedia
+                        tvResultado.setTextColor(Color.parseColor(corNaMedia))
+                        tvResultado.visibility = View.VISIBLE
+                    }
+
+                } else {
                     tvResultado.text = fraseAprovado
                     tvResultado.setTextColor(Color.parseColor(corAprovado))
                     tvResultado.visibility = View.VISIBLE
-                } else {
-                    tvResultado.text = fraseNaMedia
-                    tvResultado.setTextColor(Color.parseColor(corNaMedia))
-                    tvResultado.visibility = View.VISIBLE
                 }
-
-            } else {
-                tvResultado.text = fraseAprovado
-                tvResultado.setTextColor(Color.parseColor(corAprovado))
-                tvResultado.visibility = View.VISIBLE
             }
         }
 
